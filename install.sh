c62b7e45351cb70ca8f97d7d0a5972359181d45b
ncoonrod <ncoonrod@freelight.org>
2026-02-24 23:03:59 -0700

    chore: update;

--- install.sh
+++ install.sh
@@ -4,15 +4,10 @@ ENV=$1
 CHART=${2:-"freelight-dao"}
 VERSION=${3:-"0.5.0"}
 NAMESPACE=${4:-"$ENV"}
+APP_NAME=${5:-"$CHART"}
 
 if [ "$ENV" == "" ]; then
     echo "Please specify the environment: ./install.sh staging"
-
-    exit 1
-fi
-
-if [ ! -f "./env.$ENV/$CHART/custom-values.yaml" ]; then
-    echo "./env.$ENV/$CHART/custom-values.yaml file is missing."
 
     exit 1
 fi
@@ -31,8 +26,16 @@ if [ "$KUBECONFIG_PATH" != "" ]; then
   KUBECONFIG_STR="--kubeconfig $KUBECONFIG_PATH"
 fi
 
-echo "Installing chart $CHART for environment $ENV into namespace $NAMESPACE"
+ENV_FILE=${6:-"./env.$ENV/$CHART/custom-values.yaml"}
 
-helm upgrade $KUBECONFIG_STR --install $CHART ./charts/$CHART/$VERSION/ \
+if [ ! -f "$ENV_FILE" ]; then
+    echo "$ENV_FILE file is missing."
+
+    exit 1
+fi
+
+echo "Installing chart $CHART for environment $ENV into namespace $NAMESPACE using $ENV_FILE"
+
+helm $KUBECONFIG_STR upgrade --install $APP_NAME ./charts/$CHART/$VERSION/ \
     --namespace $NAMESPACE --create-namespace \
-    --values ./env.$ENV/$CHART/custom-values.yaml
+    --values $ENV_FILE
